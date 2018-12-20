d2c7a66b70034ed0ac2f25be3a13ed7005876a09
mre56 <mre56@scarletmail.rutgers.edu>
2018-12-19 23:28:36 -0500

    Add Project

---
+++ First Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 2f;
+    public GameObject completelevelUI;
+
+    public void CompleteLevel() {
+        completelevelUI.SetActive(true);
+    }
+
+    public void EndGame() {
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
